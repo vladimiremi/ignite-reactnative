b6e3b1d7bc67b4c5ec68bc6dd5dabe851f56f372
vladimiremi <vladimiremi2014@gmail.com>
2023-01-31 17:09:12 -0300

    feat: add Participant component

--- inhere/src/screens/Home/index.tsx
+++ inhere/src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export function Home() {
@@ -23,6 +24,9 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
+      <Participant />
+      <Participant />
     </View>
   );
 }
