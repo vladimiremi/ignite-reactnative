bfca7661628c11c3cf81468ee31590b907b67719
vladimiremi <vladimiremi2014@gmail.com>
2023-01-31 14:32:44 -0300

    refactor: add StyleSheet

--- inhere/App.tsx
+++ inhere/App.tsx
@@ -1,28 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#131016",
-        padding: 24,
-      }}
-    >
-      <Text
-        key="1"
-        style={{
-          color: "#FDFCFE",
-          fontSize: 24,
-          fontWeight: "bold",
-          marginTop: 48,
-        }}
-      >
-        React
+    <View style={styles.container}>
+      <Text key="1" style={styles.eventName}>
+        Nome do Evento
       </Text>
-      <Text key="2" style={{ color: "#6B6B6B", fontSize: 16 }}>
+      <Text key="2" style={styles.eventDate}>
         Sexta, 4 de Novembro de 2022.
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: { color: "#6B6B6B", fontSize: 16 },
+});
