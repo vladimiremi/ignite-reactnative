d22ccb9743e2ccdc8757ec415d69f5df647d5713
vladimiremi <vladimiremi2014@gmail.com>
2023-01-31 16:15:36 -0300

    refactor: insert styles from the input

--- inhere/src/screens/Home/styles.ts
+++ inhere/src/screens/Home/styles.ts
@@ -13,4 +13,12 @@ export const styles = StyleSheet.create({
     marginTop: 48,
   },
   eventDate: { color: "#6B6B6B", fontSize: 16 },
+
+  input: {
+    backgroundColor: "#1F1E25",
+    height: 56,
+    color: "#fff",
+    padding: 16,
+    fontSize: 16,
+  },
 });
