6fe13d438069325d8062e6ade062460bfb6556db
vladimiremi <vladimiremi2014@gmail.com>
2023-01-31 14:10:21 -0300

    refactor: add style elements

--- inhere/App.tsx
+++ inhere/App.tsx
@@ -2,9 +2,27 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text key="1">React</Text>
-      <Text key="2">Native</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#131016",
+        padding: 24,
+      }}
+    >
+      <Text
+        key="1"
+        style={{
+          color: "#FDFCFE",
+          fontSize: 24,
+          fontWeight: "bold",
+          marginTop: 48,
+        }}
+      >
+        React
+      </Text>
+      <Text key="2" style={{ color: "#6B6B6B", fontSize: 16 }}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
     </View>
   );
 }
