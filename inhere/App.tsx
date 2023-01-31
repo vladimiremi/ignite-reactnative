351a685fb2157af0cad57dcbe630ab4750c67d5b
vladimiremi <vladimiremi2014@gmail.com>
2023-01-31 15:34:51 -0300

    feat: rendering Home in the App

--- inhere/App.tsx
+++ inhere/App.tsx
@@ -1,29 +1,5 @@
-import { Text, View, StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text key="1" style={styles.eventName}>
-        Nome do Evento
-      </Text>
-      <Text key="2" style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24,
-  },
-  eventName: {
-    color: "#FDFCFE",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-  eventDate: { color: "#6B6B6B", fontSize: 16 },
-});
