0a8d709060cae8e4d4392ef066a1723fd3f72c9a
vladimiremi <vladimiremi2014@gmail.com>
2023-02-04 12:07:13 -0300

    chore: config google fonts

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,11 +1,22 @@
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { Group } from "@screens/Groups";
 import theme from "./src/theme";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Group />
+      {fontsLoaded ? <Group /> : <Loading />}
     </ThemeProvider>
   );
 }
