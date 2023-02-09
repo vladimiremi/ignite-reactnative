aea908b861272870a7c655e8989959719da31f57
vladimiremi <vladimiremi2014@gmail.com>
2023-02-09 13:50:25 -0300

    refactor: use ListEmpty in groups screen

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,14 +1,26 @@
 import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
+import { useState } from "react";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Group() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
-      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
-      <GroupCard title="Galera do ignite" />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={() => <GroupCard title="Galera do ignite" />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
