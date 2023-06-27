710f60457934a609546a5b99c754f18b96d7fa7e
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-06-27 08:34:09 -0300

    chore: define the application theme

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,5 +1,11 @@
 import { Groups } from "@screens/Groups";
+import theme from "./src/theme";
+import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
-  return <Groups />;
+  return(
+    <ThemeProvider theme={theme}>
+       <Groups />
+    </ThemeProvider>
+  );
 }
